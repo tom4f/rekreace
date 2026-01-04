043b1c692a0b9da3bf587df63036d188a7b54477
Tomas Kucera <tom4f@seznam.cz>
2026-01-04 10:23:37 +0100

    feat(Prices): update year

--- src/components/Bottom/Bottom.tsx
+++ src/components/Bottom/Bottom.tsx
@@ -4,7 +4,7 @@ export const Bottom = () => {
   return (
     <>
       <Header>
-        <a href='https://tomas-blog.vercel.app/'>© 1998-2025 Tomáš Kučera</a>
+        <a href='https://tomas-blog.vercel.app/'>{`© 1998-${new Date().getFullYear()} Tomáš Kučera`}</a>
         <br />
         <a href='http://www.lipnonet.cz/'>LIPNOnet.cz</a> |
         <a href='http://www.lipno.net/'> LIPNO.net</a> |
